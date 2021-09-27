a79d5a6a81178c70ba54863a6dfb59018fa1de87
Joosep Elias Tõru <joosep-elias.toru@khk.ee>
2021-09-27 11:43:16 +0300

    Switches

--- app.js
+++ app.js
@@ -1,79 +1,51 @@
 /*
-if(tingimus) {
-	// tegevused if puhul
-} else {
-	// tegevused else puhul
+const color = "r";
+
+switch(color) {
+	case "red":
+		console.log("SU S");
+		break;
+	case "yellow":
+		console.log("task electri al")
+		break;
+	case "blue":
+		console.log("agmomus")
+		break;
+	case "pink":
+		console.log("asmolgus")
+		break;
+	default:
+		console.log("AMOGඞඞඞඞUSඞඞඞඞඞඞඞඞ SUඞඞඞඞඞඞඞඞඞඞඞඞඞSSYඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ BAKAඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ ")
 }
 */
 
-const id = 100;
+const today = new Date();
 
-// == - võrdub
-// != - ei võrdu
-// === võrdub ja tüübikontroll
-// !== ei võrdu ja tüübikontroll
+nädalapäev = today.getDay();
 
-
-/*
-let testID;
-
-console.log(testID);
-
-if(typeof testID !== 'undefined'){
-	console.log(`id on ${testID}`);
-} else {
-	console.log('ID puudub');
+switch(nädalapäev) {
+	case 1:
+		console.log("täna on esmaspäev");
+		break;
+	case 2:
+		console.log("täna on teisipäev");
+		break;
+	case 3:
+		console.log("täna on kolmapäev");
+		break;
+	case 4:
+		console.log("täna on neljapäev");
+		break;
+	case 5:
+		console.log("täna on reede");
+		break;
+	case 6:
+		console.log("täna on laupäev");
+		break;
+	case 0:
+		console.log("täna on pühapäev");
+		break;
+	default:
+		console.log("taun");
+		break;
 }
-*/
-
-/*
-if(id > 100){
-	console.log(`id on ${id} - suurem kui 100`);
-} else {
-	console.log(`id on ${id} - väiksem või võrdne 100`);
-}
-*/
-
-// if - else if - else
-
-/*
-const color = "blu";
-
-if(color === "red"){
-	console.log("stop");
-} else if(color === "yello"){
-	console.log("ATentison!!!1");
-} else if (color === "greem"){
-	console.log("Go");
-} else {
-	console.log("mis kuradi ristmikul sa oled");
-}
-*/
-
-// and - &&
-const name = "Vello";
-const age = "13";
-
-if(age > 0 && age <= 16){
-	console.log(`${name} on laps`)
-} else if (age > 16 && age < 18){
-	console.log(`${name} on nooruk`)
-} else {
-	console.log(`${name} on täiskasnvanu`)
-}
-
-// or - ||
-
-if(age < 18 || age > 65){
-	console.log(`${name} ei saa registreerida`)
-} else {
-	console.log(`${name} on registreeritud`)
-}
-
-// ? - ternary operator
-
-console.log(id === 100 ? 'Õige' : 'Vale');
-
-// ilma {}
-if(id === 100) console.log('Korras');
-else console.log('Ei ole korras');
