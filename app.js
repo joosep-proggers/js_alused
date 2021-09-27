e1c962cf57cce64f9ab2463966be25d815bebfe5
Joosep Elias Tõru <joosep-elias.toru@khk.ee>
2021-09-27 11:26:47 +0300

    Conditional statements

--- app.js
+++ app.js
@@ -1,26 +1,79 @@
-let val;
+/*
+if(tingimus) {
+	// tegevused if puhul
+} else {
+	// tegevused else puhul
+}
+*/
 
-const today = new Date();
+const id = 100;
 
-val = today.getMonth(); // 0 - jaanuar
-val = today.getDate();
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime();
+// == - võrdub
+// != - ei võrdu
+// === võrdub ja tüübikontroll
+// !== ei võrdu ja tüübikontroll
 
-let birthday = new Date('04/10/2004 11:00:00');
-birthday = new Date('September 27 2001')
 
-birthday.setMonth(2); //  januar
-birthday.setDate(12);
-birthday.setFullYear(2069);
-birthday.setHours(11);
-birthday.setMinutes(32);
-birthday.setSeconds(2);
+/*
+let testID;
 
-console.log(birthday);
-console.log(val);
+console.log(testID);
+
+if(typeof testID !== 'undefined'){
+	console.log(`id on ${testID}`);
+} else {
+	console.log('ID puudub');
+}
+*/
+
+/*
+if(id > 100){
+	console.log(`id on ${id} - suurem kui 100`);
+} else {
+	console.log(`id on ${id} - väiksem või võrdne 100`);
+}
+*/
+
+// if - else if - else
+
+/*
+const color = "blu";
+
+if(color === "red"){
+	console.log("stop");
+} else if(color === "yello"){
+	console.log("ATentison!!!1");
+} else if (color === "greem"){
+	console.log("Go");
+} else {
+	console.log("mis kuradi ristmikul sa oled");
+}
+*/
+
+// and - &&
+const name = "Vello";
+const age = "13";
+
+if(age > 0 && age <= 16){
+	console.log(`${name} on laps`)
+} else if (age > 16 && age < 18){
+	console.log(`${name} on nooruk`)
+} else {
+	console.log(`${name} on täiskasnvanu`)
+}
+
+// or - ||
+
+if(age < 18 || age > 65){
+	console.log(`${name} ei saa registreerida`)
+} else {
+	console.log(`${name} on registreeritud`)
+}
+
+// ? - ternary operator
+
+console.log(id === 100 ? 'Õige' : 'Vale');
+
+// ilma {}
+if(id === 100) console.log('Korras');
+else console.log('Ei ole korras');
