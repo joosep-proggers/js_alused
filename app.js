69783a7a17220f7cced1cc37da8286abea72b059
Joosep Elias Tõru <joosep-elias.toru@khk.ee>
2021-09-28 09:17:32 +0300

    Loops

--- app.js
+++ app.js
@@ -1,51 +1,74 @@
-/*
-const color = "r";
+// for tsükkel
 
-switch(color) {
-	case "red":
-		console.log("SU S");
-		break;
-	case "yellow":
-		console.log("task electri al")
-		break;
-	case "blue":
-		console.log("agmomus")
-		break;
-	case "pink":
-		console.log("asmolgus")
-		break;
-	default:
-		console.log("AMOGඞඞඞඞUSඞඞඞඞඞඞඞඞ SUඞඞඞඞඞඞඞඞඞඞඞඞඞSSYඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ BAKAඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞඞ ")
+console.log("For tsükkel")
+for(let i = 0; i < 10; i++){
+	if(i==5){
+		// break;
+		continue;
+	}
+	console.log(i)
 }
-*/
 
-const today = new Date();
+// while tsükkel :D
 
-nädalapäev = today.getDay();
+console.log("While tsükkel");
 
-switch(nädalapäev) {
-	case 1:
-		console.log("täna on esmaspäev");
-		break;
-	case 2:
-		console.log("täna on teisipäev");
-		break;
-	case 3:
-		console.log("täna on kolmapäev");
-		break;
-	case 4:
-		console.log("täna on neljapäev");
-		break;
-	case 5:
-		console.log("täna on reede");
-		break;
-	case 6:
-		console.log("täna on laupäev");
-		break;
-	case 0:
-		console.log("täna on pühapäev");
-		break;
-	default:
-		console.log("taun");
-		break;
+let i = 0;
+
+while(i < 10){
+	console.log(i);
+	i++;
+}
+
+// do while tsükkel
+
+console.log("Do while tsükkel");
+
+let j = 0;
+do {
+	console.log(j)
+	j++;
+} while(j < 10);
+
+// tsüklid ja massiivid
+
+console.log("Massiivid ja tsüklid")
+const cars = ["Ford","Toyota","Honda"];
+
+// for tsükliga
+console.log("For tsükliga");
+console.log();
+
+for(let i=0; i < cars.length; i++){
+	console.log(cars[i]);
+};
+
+// forEach - callback function
+console.log("forEach tsükkliga");
+console.log();
+
+cars.forEach(function(element, index){
+	console.log(`cars[${index}] = ${element}`);
+});
+
+// forEach -  (arrow) => function
+console.log("forEach tsükkliga");
+console.log();
+
+cars.forEach((element, index) => {
+	console.log(`cars[${index}] = ${element}`);
+});
+
+console.log();
+
+// Tsüklid ja objektid
+const crewmate = {
+	colour: 'Red',
+	impostor: false,
+	susMeter: 57
+};
+
+// for in tsükkel
+for(let key in crewmate){
+	console.log(`${key} = ${crewmate[key]}`);
 }
